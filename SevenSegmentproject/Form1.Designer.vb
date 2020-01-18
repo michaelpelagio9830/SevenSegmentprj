1cba1ce05543a2639e1d2204ab82e94eab494d7d
michaelpelagio9830 <michaelpelagio9830@gmail.com>
2020-01-18 12:55:11 +0800

    Initial commit

---
+++ SevenSegmentproject/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Gray
+        Me.Label1.Location = New System.Drawing.Point(96, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(110, 17)
+        Me.Label1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Gray
+        Me.Label2.Location = New System.Drawing.Point(205, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(17, 90)
+        Me.Label2.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Gray
+        Me.Label3.Location = New System.Drawing.Point(205, 178)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(17, 90)
+        Me.Label3.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Gray
+        Me.Label4.Location = New System.Drawing.Point(96, 268)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(110, 17)
+        Me.Label4.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.Gray
+        Me.Label5.Location = New System.Drawing.Point(79, 178)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(17, 90)
+        Me.Label5.TabIndex = 4
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.Gray
+        Me.Label6.Location = New System.Drawing.Point(80, 67)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(17, 90)
+        Me.Label6.TabIndex = 5
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.Gray
+        Me.Label7.Location = New System.Drawing.Point(97, 160)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(110, 17)
+        Me.Label7.TabIndex = 6
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(24, 311)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(24, 354)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(171, 23)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(13, 13)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(325, 405)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label8 As Label
+End Class
---
+++ SevenSegmentproject/Form1.vb
@@ -0,0 +1,140 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label8.Text = Label8.Text + 1
+
+
+        Select Case Label8.Text
+
+            Case 1
+
+                '0
+                Label1.BackColor = Color.Red
+                Label2.BackColor = Color.Red
+                Label3.BackColor = Color.Red
+                Label4.BackColor = Color.Red
+                Label5.BackColor = Color.Red
+                Label6.BackColor = Color.Red
+
+                Label7.BackColor = Color.Black
+
+            Case 2
+
+                '1
+                Label2.BackColor = Color.Red
+                Label3.BackColor = Color.Red
+
+                Label1.BackColor = Color.Black
+                Label4.BackColor = Color.Black
+                Label5.BackColor = Color.Black
+                Label6.BackColor = Color.Black
+                Label7.BackColor = Color.Black
+
+            Case 3
+                '2
+                Label1.BackColor = Color.Red
+                Label2.BackColor = Color.Red
+                Label7.BackColor = Color.Red
+                Label5.BackColor = Color.Red
+                Label4.BackColor = Color.Red
+
+                Label3.BackColor = Color.Black
+                Label6.BackColor = Color.Black
+
+
+            Case 4
+                '3
+                Label1.BackColor = Color.Red
+                Label2.BackColor = Color.Red
+                Label7.BackColor = Color.Red
+                Label3.BackColor = Color.Red
+                Label4.BackColor = Color.Red
+
+                Label5.BackColor = Color.Black
+                Label6.BackColor = Color.Black
+
+
+            Case 5
+                '4
+                Label2.BackColor = Color.Red
+                Label3.BackColor = Color.Red
+                Label6.BackColor = Color.Red
+                Label7.BackColor = Color.Red
+
+                Label1.BackColor = Color.Black
+                Label4.BackColor = Color.Black
+                Label5.BackColor = Color.Black
+
+
+            Case 6
+                '5
+                Label1.BackColor = Color.Red
+                Label3.BackColor = Color.Red
+                Label4.BackColor = Color.Red
+                Label6.BackColor = Color.Red
+                Label7.BackColor = Color.Red
+
+                Label2.BackColor = Color.Black
+                Label5.BackColor = Color.Black
+
+
+            Case 7
+                '6
+                Label1.BackColor = Color.Red
+                Label3.BackColor = Color.Red
+                Label4.BackColor = Color.Red
+                Label5.BackColor = Color.Red
+                Label6.BackColor = Color.Red
+                Label7.BackColor = Color.Red
+
+                Label2.BackColor = Color.Black
+
+
+            Case 8
+                '7
+                Label1.BackColor = Color.Red
+                Label2.BackColor = Color.Red
+                Label3.BackColor = Color.Red
+
+                Label4.BackColor = Color.Black
+                Label5.BackColor = Color.Black
+                Label6.BackColor = Color.Black
+                Label7.BackColor = Color.Black
+
+            Case 9
+                '8
+                Label1.BackColor = Color.Red
+                Label2.BackColor = Color.Red
+                Label3.BackColor = Color.Red
+                Label4.BackColor = Color.Red
+                Label5.BackColor = Color.Red
+                Label6.BackColor = Color.Red
+                Label7.BackColor = Color.Red
+
+            Case 10
+                '9
+                Label1.BackColor = Color.Red
+                Label2.BackColor = Color.Red
+                Label3.BackColor = Color.Red
+                Label4.BackColor = Color.Red
+                Label6.BackColor = Color.Red
+                Label7.BackColor = Color.Red
+
+                Label5.BackColor = Color.Black
+
+        End Select
+
+        If (Label8.Text > 10) Then
+
+            Label8.Text = -1
+        End If
+    End Sub
+End Class
